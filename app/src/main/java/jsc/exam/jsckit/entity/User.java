23f572b3b771157d3013d05aaab2c5ebfa4ea669
JustinRoom <1006368252@qq.com>
2018-04-17 09:11:39 +0800

    add User.java

---
+++ app/src/main/java/jsc/exam/jsckit/entity/User.java
@@ -0,0 +1,72 @@
+package jsc.exam.jsckit.entity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
+    private String id;
+    private String name;
+    private int age;
+    private int sex;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    protected User(Parcel in) {
+        in.writeString(getId());
+        in.writeString(getName());
+        in.writeInt(getAge());
+        in.writeInt(getSex());
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+}
