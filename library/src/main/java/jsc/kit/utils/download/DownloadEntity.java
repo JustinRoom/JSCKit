8afd009e66f3742dd8ede46ff6602a3c0b008f94
JustinRoom <1006368252@qq.com>
2018-04-25 15:05:22 +0800

    release version:0.2.2 again

---
+++ library/src/main/java/jsc/kit/utils/download/DownloadEntity.java
@@ -0,0 +1,51 @@
+package jsc.kit.utils.download;
+
+public class DownloadEntity {
+
+    private String mimeType;
+    private String url;
+    /**the path within the external directory, including the destination filename*/
+    private String subPath;
+    private String title;
+    private String desc;
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSubPath() {
+        return subPath;
+    }
+
+    public void setSubPath(String subPath) {
+        this.subPath = subPath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
