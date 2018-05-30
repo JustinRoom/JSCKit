8c8d29999e1ce472e34ef040a6ae3f97dfecd578
JustinRoom <1006368252@qq.com>
2018-05-30 16:46:08 +0800

    add DynamicDrawableFactory

---
+++ BaseComponentLibrary/src/main/java/jsc/kit/component/utils/dynamicdrawable/DynamicDrawableFactory.java
@@ -0,0 +1,99 @@
+package jsc.kit.component.utils.dynamicdrawable;
+
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.view.View;
+
+/**
+ *
+ * <br>Email:1006368252@qq.com
+ * <br>QQ:1006368252
+ * <br>https://github.com/JustinRoom/JSCKit
+ *
+ * @author jiangshicheng
+ */
+public class DynamicDrawableFactory {
+
+    //>>>>>>>rectangle
+    public static Drawable cornerRectangleDrawable(@ColorInt int backgroundColor, float cornerRadius) {
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setCornerRadius(cornerRadius);
+        drawable.setColor(backgroundColor);
+        return drawable;
+    }
+
+    public static Drawable cornerRectangleDrawable(@ColorInt int backgroundColor, float[] cornerRadii) {
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setCornerRadii(cornerRadii);
+        drawable.setColor(backgroundColor);
+        return drawable;
+    }
+
+    public static Drawable cornerRectangleDrawable(@ColorInt int backgroundColor, float[] cornerRadii, @IntRange(from = 1) int borderWidth, @ColorInt int borderColor) {
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setCornerRadii(cornerRadii);
+        drawable.setStroke(borderWidth, borderColor);
+        drawable.setColor(backgroundColor);
+        return drawable;
+    }
+
+    //>>>>>>oval
+    public static Drawable ovalDrawable(@ColorInt int color) {
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setShape(GradientDrawable.OVAL);
+        drawable.setColor(color);
+        return drawable;
+    }
+
+    /**
+     *
+     * @param disable the drawable on component's disable state. See {@link View#isEnabled()}.
+     * @param pressed   the drawable on component's pressed state. See {@link View#isPressed()}
+     * @param selected  the drawable on component's selected state. See {@link View#isSelected()}.
+     * @param focused   the drawable on component's focused state. See {@link View#isFocused()}.
+     * @param normal    the drawable on component's normal state.
+     * @return drawable of combining all drawable on component's states.
+     */
+    public static StateListDrawable stateListDrawable(Drawable disable, Drawable pressed, Drawable selected, Drawable focused, @NonNull Drawable normal) {
+        StateListDrawable listDrawable = new StateListDrawable();
+        if (disable != null)
+            listDrawable.addState(new int[]{-android.R.attr.state_enabled}, disable);
+
+        if (pressed != null)
+            listDrawable.addState(new int[]{android.R.attr.state_pressed}, pressed);
+
+        if (selected != null)
+            listDrawable.addState(new int[]{android.R.attr.state_selected}, selected);
+
+        if (focused != null)
+            listDrawable.addState(new int[]{android.R.attr.state_focused}, focused);
+
+        listDrawable.addState(new int[]{}, normal);
+
+        return listDrawable;
+    }
+
+    public static ColorStateList colorStateList(@ColorInt int normal) {
+        return ColorStateList.valueOf(normal);
+    }
+
+    public static ColorStateList colorStateList(@ColorInt int pressed, @ColorInt int normal) {
+        return colorStateList(normal, pressed, normal, normal, normal);
+    }
+
+    public static ColorStateList colorStateList(@ColorInt int disable, @ColorInt int pressed, @ColorInt int selected, @ColorInt int focused, @ColorInt int normal) {
+        int[] colors = {disable, pressed, selected, focused, normal};
+        int[][] states = new int[5][];
+        states[0] = new int[]{-android.R.attr.state_enabled};
+        states[1] = new int[]{android.R.attr.state_pressed};
+        states[2] = new int[]{android.R.attr.state_selected};
+        states[3] = new int[]{android.R.attr.state_focused};
+        states[4] = new int[]{};
+        return new ColorStateList(states,colors);
+    }
+}
--- app/src/main/java/jsc/exam/jsckit/ui/CustomToastActivity.java
+++ app/src/main/java/jsc/exam/jsckit/ui/CustomToastActivity.java
@@ -1,23 +1,123 @@
 package jsc.exam.jsckit.ui;
 
-import android.os.Build;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialog;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import jsc.exam.jsckit.R;
+import jsc.kit.component.utils.dynamicdrawable.DynamicDrawableFactory;
 
 public class CustomToastActivity extends ABaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_custom_toast);
+//        setContentView(R.layout.activity_custom_toast);
+        FrameLayout contentView = new FrameLayout(this);
+        setContentView(contentView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         setTitle(getClass().getSimpleName().replace("Activity", ""));
+
+        TextView textView = new TextView(this);
+        textView.setTextColor(DynamicDrawableFactory.colorStateList(Color.WHITE, getResources().getColor(R.color.colorAccent)));
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+        textView.setBackground(createCircleButtonSelector());
+        textView.setText("Show");
+        int size = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, getResources().getDisplayMetrics());
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(size, size);
+        params.gravity = Gravity.CENTER;
+        contentView.addView(textView, params);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                widgetClick(v);
+            }
+        });
     }
 
-    public void widgetClick(View v){
-        int version = Build.VERSION.SDK_INT;
-        showCustomToast("Hello, I am a custom toast." + version);
+    public void widgetClick(View v) {
+        showBottomSheetDialog();
+    }
+
+    BottomSheetDialog dialog;
+
+    private void showBottomSheetDialog() {
+        if (dialog == null) {
+            int padding = getResources().getDimensionPixelSize(R.dimen.activity_vertical_margin);
+            LinearLayout layout = new LinearLayout(this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+            layout.setPadding(padding, padding * 3 / 4, padding, padding * 3 / 4);
+            //
+            LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params1.bottomMargin = padding / 4;
+            Button btnToast = new Button(this);
+            btnToast.setBackground(createButtonSelector());
+            btnToast.setTextColor(Color.WHITE);
+            btnToast.setText("Show Toast");
+            layout.addView(btnToast, params1);
+            btnToast.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                    showCustomToast("Hello, I am a custom toast.");
+                }
+            });
+            //
+            LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params2.bottomMargin = padding / 4;
+            Button btnCancel = new Button(this);
+            btnCancel.setBackground(createButtonSelector());
+            btnCancel.setTextColor(Color.WHITE);
+            btnCancel.setText("Cancel");
+            layout.addView(btnCancel, params2);
+            btnCancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+            dialog = new BottomSheetDialog(this);
+            dialog.setContentView(layout);
+        }
+        if (!dialog.isShowing())
+            dialog.show();
+    }
+
+    private Drawable createCircleButtonSelector(){
+        int colorPrimaryDark = getResources().getColor(R.color.colorPrimaryDark);
+        int colorPrimary = getResources().getColor(R.color.colorPrimary);
+        int colorAccent = getResources().getColor(R.color.colorAccent);
+
+        GradientDrawable pressed = new GradientDrawable();
+        pressed.setShape(GradientDrawable.OVAL);
+        pressed.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+        pressed.setColors(new int[]{colorPrimaryDark, colorAccent, colorPrimaryDark});
+
+        GradientDrawable normal = new GradientDrawable();
+        normal.setShape(GradientDrawable.OVAL);
+        normal.setStroke(4, colorPrimary);
+
+        return DynamicDrawableFactory.stateListDrawable(null, pressed, null, null, normal);
+    }
+
+    private Drawable createButtonSelector(){
+        int pressedColor = getResources().getColor(R.color.colorPrimaryDark);
+        int normalColor = getResources().getColor(R.color.colorAccent);
+        float cornerRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics());
+        Drawable pressed = DynamicDrawableFactory.cornerRectangleDrawable(pressedColor, cornerRadius);
+        Drawable normal = DynamicDrawableFactory.cornerRectangleDrawable(normalColor, cornerRadius);
+        return DynamicDrawableFactory.stateListDrawable(null, pressed, null, null, normal);
     }
 }
